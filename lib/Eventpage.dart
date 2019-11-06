3bb4f919efae8563c60a1510f8bb39aec83e9fa0
Toni <toni.boeswetter@outlook.de>
2019-11-06 23:42:05 +0100

    http response test

--- lib/Eventpage.dart
+++ lib/Eventpage.dart
@@ -1,37 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:lobevent/Models/Event.dart';
+import 'package:http/http.dart' as http;
 
-class EventList extends StatefulWidget{
+class EventList extends StatefulWidget {
   @override
-  State<StatefulWidget> createState(){
+  State<StatefulWidget> createState() {
     return new EventListState();
   }
 }
 
-class EventListState extends State<EventList>{
+class EventListState extends State<EventList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(
-        title: Text('Events'),
-      ),
-      body: _textfield()
-    );
+        appBar: new AppBar(
+          title: Text('Events'),
+        ),
+        body: _textfield());
   }
-  Widget _buildEventList(){
+
+  Widget _buildEventList() {
     return new ListView.builder(
         padding: EdgeInsets.all(10.0),
-        itemBuilder: (context, index){
-          return ListTile(
-
-          );
-        }
-    );
-
+        itemBuilder: (context, index) {
+          return ListTile();
+        });
   }
-  Widget _textfield(){
+
+  Widget _textfield() {
     return Container(
-      child: Text(),
-    );
+        child: Text(http.get('https:/192.168.0.141').toString()
+            //response = await http.get('https:/192.168.0.141'
+            ));
   }
 }

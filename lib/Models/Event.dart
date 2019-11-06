ede162a0261c1389915cbd87090eb5127ef2b9a0
Tom Prantz <t.prantz@gmx.de>
2019-11-06 22:21:39 +0100

    models event from json

---
+++ lib/Models/Event.dart
@@ -0,0 +1,14 @@
+class Event {
+  final int id;
+  final String name;
+  final String date;
+
+  Event({this.id, this.name, this.date});
+  factory Event.fromJson(Map<String, dynamic> json) {
+    return Event(
+      id: json['id'] as int,
+      name: json['name'] as String,
+      date: json['date'] as String,
+    );
+  }
+}

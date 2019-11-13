f4d32b6e73027eeda080ed3fdb8a159fb7610a09
Tom Prantz <t.prantz@gmx.de>
2019-11-13 23:13:53 +0100

    eventList models

--- lib/Models/Event.dart
+++ lib/Models/Event.dart
@@ -1,3 +1,18 @@
+class EventsList {
+  final List<Event> events;
+
+  EventsList(
+    this.events,
+  );
+
+  factory EventsList.fromJson(List<dynamic> parsedJson) {
+    List<Event> events = new List<Event>();
+    events = parsedJson.map((i) => Event.fromJson(i)).toList();
+
+    return new EventsList(events);
+  }
+}
+
 class Event {
   final int id;
   final String name;
@@ -11,5 +26,4 @@ class Event {
       date: json['date'] as String,
     );
   }
-
 }

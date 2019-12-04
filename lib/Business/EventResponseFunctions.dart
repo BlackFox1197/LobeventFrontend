bb1aceff3b35922932d21fcf9535032a309d3659
Daniel Lenhardt <ckwdani@gmail.com>
2019-12-04 22:14:40 +0100

    Adding EventResponseFunctions in Bussness

---
+++ lib/Business/EventResponseFunctions.dart
@@ -0,0 +1,20 @@
+import 'package:lobevent/Data/Types/UserEventStatus.dart';
+import 'package:lobevent/Services/Communication/EventCommunicator.dart';
+import 'package:lobevent/Services/Communication/UserEventStatusCommunicator.dart';
+
+class EventFunctions {
+
+  UserEventStatusCommunicator communicator;
+  EventFunctions(){
+    this.communicator = new UserEventStatusCommunicator();
+  }
+
+  ///generates UserEventStatus response
+  ///$response should be between 0 and 2 where 0 is accept, 1 is deny, 2 is interested
+  void sendEventResponse(int response, int eventId){
+    UserEventStatus userEventStatus = UserEventStatus();
+    userEventStatus.eventId = eventId;
+    userEventStatus.status = response;
+    communicator.post(userEventStatus);
+  }
+}

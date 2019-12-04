2b40f0142a5f161dd691f28c15bac2d562ca1015
Daniel Lenhardt <ckwdani@gmail.com>
2019-12-04 20:31:27 +0100

    add userId to eventType

--- lib/Data/Types/Event.dart
+++ lib/Data/Types/Event.dart
@@ -4,21 +4,24 @@ class Event extends MyType{
   final int id;
   final String name;
   final DateTime date;
+  final int userId;
 
-  Event({this.id, this.name, this.date});
+  Event({this.id, this.name, this.date, this.userId});
 
   factory Event.fromJson(Map<String, dynamic> json) {
     return Event(
       id: json['id'] as int,
       name: json['name'] as String,
       date: json['date'] as DateTime,
+      userId: json['userId'] as int
     );
   }
   Map<String, dynamic> toJson() =>
       {
         'name' : name,
         'date' : date,
-        'id'   : id
+        'id'   : id,
+        'userId': userId
       };
 
 }
